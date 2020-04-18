8c128d9e248b60451ec2047812edf7505b2143a4
Lisa Chandra <liza761c@gmail.com>
2020-04-18 19:47:26 +0530

    add argument to the swtichNameHandler for changing names based on it

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
@@ -29,7 +29,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Lisa')}>Switch Name</button>
+        {/* This here is what will be refered to for the this (this.setState) inside the handler function */}
+        {/* <button onClick={() => this.switchNameHandler('Lisa!!')}>Switch Name</button> */}
+        {/* This method is considered inefficient though and is not recommended. */}
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -37,6 +40,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Mansi')}
         >
           My Hobbies: Racing
         </Person>
