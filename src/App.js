d68efd400fbb02b50c9829b2f8168b381fccb830
Lisa Chandra <liza761c@gmail.com>
2020-04-24 21:05:06 +0530

    modified nameChangedHandler for the dynamic list of persons

--- src/App.js
+++ src/App.js
@@ -23,14 +23,22 @@ class App extends Component {
     });
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    // OR
+    // const person = Onject.assign({}, this.state.persons[personIndex]) (old)
+    person.name = event.target.value;   
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -63,10 +71,8 @@ class App extends Component {
               click = {() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key = {person.id} />
-              // key added, because react keeps a virtual DOM to compare the changes it makes.
-              // Therefore, it uses this to compare elements. This will help remove the 
-              // previous key error. This key has to be something unique.
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './Person.css'
-// Reacts makes it possible to import CSS files, it doesn't work without importing as no files is added to the
-// build workflow unless its imported.
-// The style tags are added dynamically by webpack. It manages the imports you add.
-// It also adds the additional webkit stuff to get your stylings to work in many browsers
 
 const person = (props) => {
     return (
@@ -11,7 +7,6 @@ const person = (props) => {
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-            {/* for some reason the input type text doe snot work if there is not onChanged but there is a value */}
         </div>
     );
 }
