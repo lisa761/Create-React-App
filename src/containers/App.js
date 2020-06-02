689db489145fc2b449ee08de1df10443bc1cec28
Lisa Chandra <liza761c@gmail.com>
2020-06-02 20:16:50 +0530

    added props to app component through index.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -66,7 +66,8 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <Cockpit
+          title={this.props.appTitle} 
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
